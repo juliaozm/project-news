f07d40334501a365d35f23c44b7bd18a04be4c32
juliazom <ozmiteljul@gmail.com>
2023-01-20 11:12:38 +0000

    Set up a production database

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,12 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+const config = ENV === 'production'
+  ? { connectionString: process.env.DATABASE_URL, max: 2 }
+  : {};
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
---
+++ listen.js
@@ -0,0 +1,6 @@
+const app = require('./app.js')
+const { PORT = 9090 } = process.env
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`)
+})
