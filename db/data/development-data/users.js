550aad445ece9bbcd581c08b947b06cc53a77dd0
juliazom <ozmiteljul@gmail.com>
2023-06-03 17:36:28 +0100

    Added "email" in databases

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,38 +1,38 @@
 module.exports = [
   {
-    username: 'tickle122',
-    name: 'Tom Tickle',
+    email: "tickle122@gmail.com",
+    username: "tickle122",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953'
+      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
   },
   {
-    username: 'grumpy19',
-    name: 'Paul Grump',
+    email: "grumpy19@gmail.com",
+    username: "grumpy19",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013'
+      "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
   },
   {
-    username: 'happyamy2016',
-    name: 'Amy Happy',
+    email: "happyamy2016@gmail.com",
+    username: "happyamy2016",
     avatar_url:
-      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'
+      "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
   },
   {
-    username: 'cooljmessy',
-    name: 'Peter Messy',
+    email: "cooljmessy@gmail.com",
+    username: "cooljmessy",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002'
+      "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
   },
   {
-    username: 'weegembump',
-    name: 'Gemma Bump',
+    email: "weegembump@gmail.com",
+    username: "weegembump",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553'
+      "https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553",
   },
   {
-    username: 'jessjelly',
-    name: 'Jess Jelly',
+    email: "jessjelly@gmail.com",
+    username: "jessjelly",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141'
-  }
+      "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
+  },
 ];
--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -1,23 +1,36 @@
 module.exports = [
   {
+    email: "butter_bridge@gmail.com",
     username: "butter_bridge",
-    name: "jonny",
     avatar_url:
       "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
   },
   {
+    email: "icellusedkars@examp-le.com",
     username: "icellusedkars",
-    name: "sam",
     avatar_url: "https://avatars2.githubusercontent.com/u/24604688?s=460&v=4",
   },
   {
+    email: "rogersop@gmail.com",
     username: "rogersop",
-    name: "paul",
     avatar_url: "https://avatars2.githubusercontent.com/u/24394918?s=400&v=4",
   },
   {
+    email: "lurker@gmail.com",
     username: "lurker",
-    name: "do_nothing",
+    avatar_url:
+      "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+  },
+  {
+    email: "an.ony-mo_us@gmail.com",
+    username: "anonanon2222",
+    avatar_url:
+      "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+  },
+
+  {
+    email: "example@subdomain.example.com",
+    username: "your_true_example",
     avatar_url:
       "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
   },
