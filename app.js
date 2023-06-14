aa587aa6d874aedc4ef7b0a423284a9d0b78a265
juliazom <ozmiteljul@gmail.com>
2023-06-14 12:39:53 +0100

    Updated cors credential in app.js

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ const {
 } = require("./controllers/controllers.js");
 const cookieParser = require("cookie-parser");
 
-app.use(cors());
-app.use(cookieParser());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" || "*" }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/api", getAllEndpoints);
 app.get("/api/topics", getTopics);
