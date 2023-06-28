26c4ffefa8ce772f45deb1d0dd3e4053dac5a666
juliazom <ozmiteljul@gmail.com>
2023-06-28 23:44:43 +0100

    Changed exp time of token

--- models/jwt-helpers.js
+++ models/jwt-helpers.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const jwtTokens = ({ email, username, avatar_url }) => {
   const user = { email, username, avatar_url };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "3m",
+    expiresIn: "30s",
   });
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: "15m",
